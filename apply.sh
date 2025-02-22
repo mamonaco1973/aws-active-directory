9a8712c3d1fa7bf4146049ca3a4835f2b5ef246a
mamonaco1973 <mamonaco1973@gmail.com>
2025-02-22 15:02:55 +0000

    work on shell scripts

--- apply.sh
+++ apply.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
+# Check to make sure we can build
+
 ./check_env.sh
 if [ $? -ne 0 ]; then
   echo "ERROR: Environment check failed. Exiting."
   exit 1
 fi
 
+# Build Phase 1 - Create the AD instance
+
+cd 01-directory
+
 terraform init
 terraform apply -auto-approve
+
+cd ..
+
+# Build Phase 2 - Create EC2 Instances
+
+cd 02-servers
+
+terraform init
+terraform apply -auto-approve
+
+cd .. 
+
--- destroy.sh
+++ destroy.sh
@@ -1,5 +1,28 @@
 
 #!/bin/bash
 
+# Phase 1 of Destroy - delete EC2 instances
+
+cd 02-servers
+
 terraform init
 terraform destroy -auto-approve
+
+cd ..
+
+# Phase 2 of Destroy - delete AD instance
+
+# Force secret deletion
+
+aws secretsmanager delete-secret --secret-id "akumar_ad_credentials" --force-delete-without-recovery
+aws secretsmanager delete-secret --secret-id "jsmith_ad_credentials" --force-delete-without-recovery
+aws secretsmanager delete-secret --secret-id "edavis_ad_credentials" --force-delete-without-recovery
+aws secretsmanager delete-secret --secret-id "rpatel_ad_credentials" --force-delete-without-recovery
+aws secretsmanager delete-secret --secret-id "admin_ad_credentials" --force-delete-without-recovery
+
+cd 01-directory
+
+terraform init
+terraform destroy -auto-approve
+
+
