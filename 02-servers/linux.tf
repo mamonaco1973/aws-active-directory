39ede3e6e6539f26b11c4a615dab38c8f9e4c0d4
Mike Monaco <mamonaco1973@gmail.com>
2025-03-06 12:15:19 -0500

    fix for new domain_fqdn

--- 02-servers/linux.tf
+++ 02-servers/linux.tf
@@ -56,7 +56,7 @@ resource "aws_instance" "linux_ad_instance" {
 
   user_data = templatefile("./scripts/userdata.sh", { 
     admin_secret = "admin_ad_credentials"                       # The administrator credentials secret
-    domain_fqdn  = "mikecloud.com"                              # The domain FQDN for Active Directory integration.
+    domain_fqdn  = "mcloud.mikecloud.com"                       # The domain FQDN for Active Directory integration.
     computers_ou = "OU=Computers,OU=MCLOUD,DC=mcloud,DC=mikecloud,DC=com" # The AD OU where computers will be placed.
   })
 
